f9c1ee1e2b55823aedf811a2df925575148da343
z-start <zzxstar@vip.qq.com>
2015-12-13 20:44:26 +0800

    commit update

--- app.js
+++ app.js
@@ -5,29 +5,24 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var routes = require('./routes/index');
-var users = require('./routes/users');
-var work = require('./routes/work');
-var product = require('./routes/product');
 var app = express();
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
 // uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public' ,'images','favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
-app.use('/users', users);
-app.use('/work', work);
-app.use('/product', product);
 app.use('/w', express.static(path.join(__dirname, 'public/work.html')));
 app.use('/p', express.static(path.join(__dirname, 'public/products.html')));
 app.use('/404', express.static(path.join(__dirname, 'public/404.html')));
 app.use('/contact', express.static(path.join(__dirname, 'public/contact.html')));
+app.use('/v', express.static(path.join(__dirname, 'public/view.html')));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
